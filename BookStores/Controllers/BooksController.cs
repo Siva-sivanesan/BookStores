1ff315c0703288c1308f0522a9587188b8448a1b
yogi <yogi@DESKTOP-8P9N5JH>
2022-04-20 17:44:23 +0530

    Add project files.

---
+++ BookStores/Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using BookStores.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Controllers
+{
+    public class BooksController : Controller
+    {
+        public int ID { get; private set; }
+        public string Title { get; private set; }
+        public string Genre { get; private set; }
+        public int Price { get; private set; }
+        public DateTime PublishDate { get; private set; }
+        public List<string> Authors { get; private set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            Book book = new Book()
+            {
+                ID = 1,
+                Title = "Learning ASP.NET Core 2.0",
+                Genre = "Programming and software development",
+                Price = 45,
+                PublishDate = new System.DateTime(2022, 04, 06),
+                Authors = new List<string> { "William Shakespeare, Oscar Wilde" }
+            };
+            return View(book);
+        }
+    }
+}
---
+++ BookStores/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStores.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        [Required]
+        [StringLength(maximumLength: 20, ErrorMessage = "The title length should be between 2 and 20.", MinimumLength=2)]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public List<string> Authors{ get; set; }
+        public decimal Price { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
